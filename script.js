c764ebed55c4891f83964f0866d4cdff3beace8b
Madhesh005 <madheshr005@gmail.com>
2024-07-27 16:35:31 +0530

    Final edit completed

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 var navbar_mob = document.querySelector(".navbar-mob");
 
-function mob(){
+function mob() {
     navbar_mob.style.left = "0%"
 }
 
-function closenav(){
+function closenav() {
     navbar_mob.style.left = "-80%"
 }
 
+var enteredtext = ""
+var gallery = document.querySelector(".productgallery");
+var tosearch = gallery.querySelectorAll("div");
+console.log(tosearch);
+
+function check(event) {
+    enteredtext = event.target.value.toUpperCase()
+    for (var i = 0; i < tosearch.length; i++) {
+        if (tosearch[i].textContent.toUpperCase().indexOf(enteredtext) < 0) {
+            tosearch[i].style.display = "none"
+        }
+        else{
+            tosearch[i].style.display="block"
+        }
+    }
+}
